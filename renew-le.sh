5667881aa631a02a01fb035492c70d71a286b385
Martin Bergman <misse@ipa0.misse.lan>
2018-08-29 18:30:38 +0200

    added distro support

--- renew-le.sh
+++ renew-le.sh
@@ -1,16 +1,55 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+EMAIL="martin@devsed.se"
 set -o nounset -o errexit
+FIRSTTIME=false
+HELP=false
 
-WORKDIR="/root/ipa-le"
-EMAIL=""
-#cd "$WORKDIR"
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  
+  case $key in
+    -d|--distro)
+    DISTRIB="${2,,}"
+    shift # past argument
+    shift # past value
+    ;;
+    -f|--first-time)
+    FIRSTTIME=true
+    shift # past argument
+    ;;
+    -h|--help)
+    HELP=true
+    shift # past argument
+    ;;
+  esac
+done
+
+if [[ $HELP == true ]]
+then
+  echo "Usage: $0 -d|--distro (Ubuntu|Debian|Centos|Rhel) [-f|--firsttime] [-h|--help]"
+  exit 1
+fi
+
+
+WORKDIR=$(pwd)
+if [ $DISTRIB == 'ubuntu' ] || [ $DISTRIB = 'debian' ]; then
+  APACHEDIR="/etc/apache2/nssdb"
+  APACHESVC="apache2"
+elif [ $DISTRIB == 'centos'] || [ $DISTRIB = 'rhel' ]; then
+  APACHEDIR="/etc/httpd/alias"
+  APACHESVC="httpd"
+else
+  echo "-d|--distro must be set to one of the following: Ubuntu Debian Centos Rhel"
+  exit 1
+fi
 
 ### cron
 # check that the cert will last at least 2 days from now to prevent too frequent renewal
 # comment out this line for the first run
-if [ "${1:-renew}" != "--first-time" ]
+if  [[ $FIRSTTIME = false ]]
 then
-	certutil -d /etc/httpd/alias/ -V -u V -n Server-Cert -b "$(date '+%y%m%d%H%M%S%z' --date='2 days')" && exit 0
+	certutil -d $APACHEDIR/ -V -u V -n Server-Cert -b "$(date '+%y%m%d%H%M%S%z' --date='2 days')" && exit 0
 fi
 
 # cert renewal is needed if we reached this line
@@ -20,18 +59,18 @@ rm -f "$WORKDIR"/*.pem
 rm -f "$WORKDIR"/httpd-csr.*
 
 # generate CSR
-certutil -R -d /etc/httpd/alias/ -k Server-Cert -f /etc/httpd/alias/pwdfile.txt -s "CN=$(hostname -f)" --extSAN "dns:$(hostname -f)" -o "$WORKDIR/httpd-csr.der"
+certutil -R -d $APACHEDIR/ -k Server-Cert -f $APACHEDIR/pwdfile.txt -s "CN=$(hostname -f)" --extSAN "dns:$(hostname -f)" -o "$WORKDIR/httpd-csr.der"
 
-# httpd process prevents letsencrypt from working, stop it
-service httpd stop
+# $APACHESVC process prevents letsencrypt from working, stop it
+service $APACHESVC stop
 
 # get a new cert
 letsencrypt certonly --standalone --csr "$WORKDIR/httpd-csr.der" --email "$EMAIL" --agree-tos
 
 # remove old cert
-certutil -D -d /etc/httpd/alias/ -n Server-Cert
+certutil -D -d $APACHEDIR/ -n Server-Cert
 # add the new cert
-certutil -A -d /etc/httpd/alias/ -n Server-Cert -t u,u,u -a -i "$WORKDIR/0000_cert.pem"
+certutil -A -d $APACHEDIR/ -n Server-Cert -t u,u,u -a -i "$WORKDIR/0000_cert.pem"
 
-# start httpd with the new cert
-service httpd start
+# start $APACHESVC with the new cert
+service $APACHESVC start
--- setup-le.sh
+++ setup-le.sh
@@ -1,9 +1,41 @@
 #!/usr/bin/bash
 set -o nounset -o errexit
+HELP=false
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  
+  case $key in
+    -d|--distro)
+    DISTRIB="${2,,}"
+    shift # past argument
+    shift # past value
+    ;;
+    -h|--help)
+    HELP=true
+    shift # past argument
+    ;;
+  esac
+done
 
-WORKDIR="/root/ipa-le"
+if [[ $HELP == true ]]
+then
+  echo "Usage: $0 -d|--distro (Ubuntu|Debian|Centos|Rhel) [-f|--firsttime] [-h|--help]"
+  exit 1
+fi
 
-dnf install letsencrypt -y
+
+WORKDIR=$(pwd)
+
+if [ $DISTRIB == 'ubuntu' ] || [ $DISTRIB = 'debian' ]; then
+  PM="apt-get"
+elif [ $DISTRIB == 'centos'] || [ $DISTRIB = 'rhel' ]; then
+  PM="dnf"
+else
+  echo "-d|--distro must be set to one of the following: Ubuntu Debian Centos Rhel"
+  exit 1
+fi
+$PM install letsencrypt -y
 
 ipa-cacert-manage install "$WORKDIR/ca/DSTRootCAX3.pem" -n DSTRootCAX3 -t C,,
 ipa-certupdate -v
@@ -11,4 +43,4 @@ ipa-certupdate -v
 ipa-cacert-manage install "$WORKDIR/ca/LetsEncryptAuthorityX3.pem" -n letsencryptx3 -t C,,
 ipa-certupdate -v
 
-"$(dirname "$0")/renew-le.sh" "--first-time"
+"$(dirname "$0")/renew-le.sh" -d $DISTRIB --first-time
